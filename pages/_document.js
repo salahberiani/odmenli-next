88453d980604b755657ac2199d3b8ccc72bce993
salahberiani <salaheddinberiani@gmail.com>
2021-10-05 10:42:48 +0100

    robots

--- pages/_document.js
+++ pages/_document.js
@@ -18,9 +18,23 @@ class Document extends NextDocument {
           <meta name="robots" content="index, follow" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="Arabic"></meta>
+          <meta
+            name="description"
+            content="نظرا للاحتيالات التجارية التي تحدث عبر مواقع الشبكة العنكبوتية بشكل متكرر وكبير وللقظاء عليها والحد منها قمنا بانشاء موقع اظمنلي الذي يظمن حدوث المبادلات التجارية دون الوقوع في فخ الاحتيال الالكتروني الذي يقوم به اشخاص بحسابات مزيفة"
+          />
           {/* <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" /> */}
+          <link rel="canonical" href="https://www.odmenli.com" />
+          <meta property="og:url" content="https://www.odmenli.com" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="أضمنلي" />
+          <meta
+            property="og:description"
+            content="نظرا للاحتيالات التجارية التي تحدث عبر مواقع الشبكة العنكبوتية بشكل متكرر وكبير وللقظاء عليها والحد منها قمنا بانشاء موقع اظمنلي الذي يظمن حدوث المبادلات التجارية دون الوقوع في فخ الاحتيال الالكتروني الذي يقوم به اشخاص بحسابات مزيفة"
+          />
+          <meta property="og:image" content="https://www.odmenli.com/main.png" />
+
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
